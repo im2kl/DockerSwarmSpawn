1781202c2ced6c234e33a3d1253ed85ff26842f0
Luis Santos <luisfcs@hotmail.co.uk>
2022-03-20 21:17:25 +0000

    Update grep
    update grep expression

--- RequirementsInstall.sh
+++ RequirementsInstall.sh
@@ -8,7 +8,7 @@ REQUIRED_PKG="gnupg2 software-properties-common apt-transport-https ca-certifica
 #Iterate and install package
 for pkg in $REQUIRED_PKG; do
     echo "> Checking $pkg"
-    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $pkg|grep "install ok installed")
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $pkg|grep -E 'install ok installed')
     echo Checking for $pkg: $PKG_OK
     if [ "" = "$PKG_OK" ]; then
     echo ">> $pkg NotFound.\n Requesting Install for: $pkg."
