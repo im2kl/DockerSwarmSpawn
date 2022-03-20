17acf6c2c0596b3f03797b60c0b784ed0786bec1
Luis Santos <luisfcs@hotmail.co.uk>
2022-03-20 20:49:38 +0000

    Docker Requirement
    Install required packages to prepare OS for docker installation.

---
+++ RequirementsInstall.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo apt update
+
+## Require Packages
+REQUIRED_PKG="gnupg2 software-properties-common apt-transport-https ca-certificates"
+
+#Iterate and install package
+for pkg in $REQUIRED_PKG; do
+    echo "> Checking $pkg"
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $pkg|grep "install ok installed")
+    echo Checking for $pkg: $PKG_OK
+    if [ "" = "$PKG_OK" ]; then
+    echo ">> $pkg NotFound.\n Requesting Install for: $pkg."
+    sudo apt --yes --no-install-recommends install $pkg
+    fi
+done
