d624e0ac28dfa4ba67897c5b38512d229015858e
Luis Santos <luisfcs@hotmail.co.uk>
2022-05-29 20:46:26 +0100

    Compose install path
    Change composer to install under userbin

--- DockerComposeInstall.sh
+++ DockerComposeInstall.sh
@@ -4,6 +4,6 @@ sudo apt --yes --no-install-recommends install curl
 mkdir -p ~/.docker/cli-plugins/
 
 # try downlaod via wget to minimize reqs
-curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-chmod +x ~/.docker/cli-plugins/docker-compose
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo chown $USER /var/run/docker.sock
